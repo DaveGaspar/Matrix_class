a9147c93d64b0749db72a74c957607ce15eced2a
DaveGaspar <dgasparyan073@gmail.com>
2023-06-09 00:02:26 +0400

    Main

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+class matrix{
+	private:
+		int row, column;
+		int** arr_2d;
+	public:
+		matrix(){
+			row = 3;
+			column = 3;
+			arr_2d = new int*[row];
+			for(int i = 0; i < row; i++)
+				arr_2d[i] = new int[column];
+		}
+		matrix(int const n, int const m){
+			row = n;
+			column = m;
+			arr_2d = new int*[row];
+			for(int i = 0; i < row; i++)
+				arr_2d[i] = new int[column];
+		}
+		~matrix(){
+			for(int i = 0; i < row; i++){
+				delete[] arr_2d[i];
+				arr_2d[i] = nullptr;
+			}
+			delete[] arr_2d;
+		}
+		void init(){
+			for(int i = 0; i < row; i++)
+				for(int j = 0; j < column; j++)
+					arr_2d[i][j] = rand() % 90 + 10;
+		}
+		void print(){
+			for(int i = 0; i < row; i++){
+				for(int j = 0; j < column; j++)
+					std::cout << arr_2d[i][j] << "\t";
+				std::cout << std::endl;
+			}
+		}
+};
+
+int main(){
+	matrix* obj1 = new matrix;
+	obj1->init();
+	obj1->print();
+	
+	int* n = new int;
+	int* m = new int;
+	
+	do{
+		std::cout << "Row = ";
+		std::cin >> *n;
+	} while(*n < 1);
+	do{
+		std::cout << "Column = ";
+		std::cin >> *m;
+	} while(*m < 1);
+
+
+	matrix* obj2 = new matrix(*n,*m);
+	obj2->init();
+	obj2->print();
+
+
+	delete obj1;
+	obj1 = nullptr;
+
+	delete obj2;
+	obj2 = nullptr;
+
+	return 0;
+}
